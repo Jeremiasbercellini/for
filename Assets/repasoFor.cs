07409c0f18fbc236c51d9e367161bc9a65cfa2f9
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-06-18 09:04:55 -0300

    repaso for y array

---
+++ Assets/repasoFor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repasoFor : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        //for (int i = 2; i <= 40; i += 2)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        //for (int i = num1; i < num2; i += num1)
+        //{
+        //    Debug.Log(i);
+        //}
+
+        //for (int i = 100; i>=10; i--)
+        //{
+        //    if (i == 33)
+        //    {
+        //        continue;
+        //    }
+        //    Debug.Log(i);
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
